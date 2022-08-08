76176c92ffa09ed57c22401d2af705d05511f4ed
swapnil-nilawar <110835226+swapnil-nilawar@users.noreply.github.com>
2022-08-08 19:58:07 +0530

    Update streamlit_app.py
    Added some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title("We love you Neeva")
 
 streamlit.header("You are blessing in our life")
+
+streamlit.text("You were born on 23rd Jan 2022")
+streamlit.text("We were very happy to see you for the first time")
+streamlit.text("Neeva, remember, your parents love you a lot!")
