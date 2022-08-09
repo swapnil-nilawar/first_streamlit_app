4c191d35f53c276482660dc3d8b191a95a417d12
swapnil-nilawar <110835226+swapnil-nilawar@users.noreply.github.com>
2022-08-09 21:40:49 +0530

    Update streamlit_app.py
    Edited code to display frutyvice response in json format

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,5 +28,8 @@ else:
   streamlit.dataframe(my_fruit_list)
 
 import requests
+
+streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json)
+
