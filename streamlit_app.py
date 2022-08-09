85e3022db885bb43046094341af241087d7f3d29
swapnil-nilawar <110835226+swapnil-nilawar@users.noreply.github.com>
2022-08-09 23:35:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,5 +50,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.text('Thanks for adding'+ fruit_choice)
+streamlit.text('Thanks for adding '+ fruit_choice)
 
