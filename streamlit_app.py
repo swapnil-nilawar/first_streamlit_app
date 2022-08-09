5f08254060f50a722fe99d93ea49d1724d674d53
swapnil-nilawar <110835226+swapnil-nilawar@users.noreply.github.com>
2022-08-10 00:01:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ try:
     #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     #streamlit.dataframe(fruityvice_normalized)
-    back_from_function = get_fruityvice_data(fruit_choice):
+    back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
       
 except URLError as e:
